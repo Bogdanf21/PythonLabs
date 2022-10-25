8f19a91730d142bad1f3634919fc36c1a968a411
Bogdanf21 <79158552+Bogdanf21@users.noreply.github.com>
2022-10-25 17:59:02 +0300

    3 and 5 finished

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -18,7 +18,14 @@ def p2(string):
 
 
 def p3(dictionary1, dictionary2):
-    print("Hello world")
+    for item1, item2 in dictionary1, dictionary2:
+        if type(item1) != type(item2):
+            return False
+        if type(item1) == type(item2) and item1 is dict or item1 is list or item1 is set:
+            return p3(item1, item2)
+        if item1 != item2:
+            return False
+    return True
 
 
 def p4(tag, content, **pairs):
@@ -29,9 +36,37 @@ def p4(tag, content, **pairs):
     return string
 
 
-def p5():
-    print("Hello world")
+def p5(set_of_rules, dictionary):
+    for key in dictionary:
+        appears = False
+        rule = ()
+        for tuple_ in set_of_rules:
+            if key == tuple_[0]:
+                appears = True
+                rule = tuple_
 
+        if not appears:
+            print(f"key {key} not in rules")
+        else:
+            string = dictionary[key]
+            if string.find(rule[1]) != 0 and len(rule[1]):
+                   print(f"sentence not respecting the first rule of this key : \n first word in sentence\n")
+
+            if string.find(rule[2]) < 0 and len(rule[2]):
+                print(
+                    f"sentence not respecting the middle rule of this key : \n word that should appear in sentence but does not appear : {rule[2]}")
+
+            if string.find(rule[2]) >= len(string) - len(rule[2]) and len(rule[2]):
+                print(
+                    f"sentence not respecting the middle rule of this key : \n word that should appear in sentence in the middle but appears on the end : {rule[2]}")
+
+            if string.find(rule[3]) < 0 and len(rule[3]):
+                print(
+                    f"sentence not respecting the last rule of this key : \n word that should appear in sentence last but it doesn't appear at all : {rule[3]}")
+
+            if string.find(rule[3], len(string) - len(rule[3])) != len(string) - len(rule[3]) and len(rule[3]):
+                print(
+                    f"sentence not respecting the last rule of this key : \n word that should appear in sentence last but it doesn't: {rule[3]}")
 
 def p6(a_list: []):
     duplicates = set()
@@ -51,16 +86,12 @@ def p7(*sets):
         print("Cannot return dictionary: set does not have an even card.")
         return
     for i in range(0, len(sets), 2):
-
-        a_intersected_b, a_reunited_b, a_minus_b, b_minus_a = p1(list(sets[i]), list(sets[i+1]))
-        dictionary[f'{sets[i]} | {sets[i+1]}'] = a_reunited_b
-        dictionary[f'{sets[i]} $ {sets[i+1]}'] = a_intersected_b
+        a_intersected_b, a_reunited_b, a_minus_b, b_minus_a = p1(list(sets[i]), list(sets[i + 1]))
+        dictionary[f'{sets[i]} | {sets[i + 1]}'] = a_reunited_b
+        dictionary[f'{sets[i]} $ {sets[i + 1]}'] = a_intersected_b
         dictionary[f'{sets[i]} - {sets[i + 1]}'] = a_minus_b
-        dictionary[f'{sets[i+1]} - {sets[i]}'] = b_minus_a
+        dictionary[f'{sets[i + 1]} - {sets[i]}'] = b_minus_a
     return dictionary
-
-
-
 
 
 def p8(dictionary):
@@ -80,8 +111,6 @@ def p9(*positional_arguments, **keyword_arguments):
     return len(list(a for a in positional if a in keyword))
 
 
-
-
 def main():
     # p1: sets a,b -> aUb a^b a/b b/a
     a = [1, 2, 3]
@@ -98,6 +127,11 @@ def main():
           p4("a", "Hello There", href=" http://python.org ", _class=" my-link ", id=" someid"))
 
     # p5
+    s = {("key1", "", "inside", "out"), ("key2", "start", "middle", "winter")}
+    d = {
+        "key1": "come,inside it's too cold out", "key3": "this is not valid"}
+    print("(5)")
+    p5(s, d)
 
     # p6:
     a_list = [1, 2, 2, 3]
@@ -114,7 +148,6 @@ def main():
     # a variable number of keyword arguments adn will return the number
     # of positional arguments whose values can be found among keyword arguments values.
     print("(9)", p9(1, 2, 3, 4, x=1, y=2, z=3, w=5))
-
 
 
 if __name__ == "__main__":
